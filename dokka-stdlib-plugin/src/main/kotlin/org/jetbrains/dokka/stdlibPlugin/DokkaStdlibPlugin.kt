889d415ec29fbe8eef79f405df1a9e094a1ef6a9
Andrzej Ratajczak <andrzej.ratajczak98@gmail.com>
2020-09-18 14:31:18 +0200

    Add old merger for stdlib

---
+++ dokka-stdlib-plugin/src/main/kotlin/org/jetbrains/dokka/stdlibPlugin/DokkaStdlibPlugin.kt
@@ -0,0 +1,13 @@
+package org.jetbrains.dokka.stdlibPlugin
+
+import org.jetbrains.dokka.CoreExtensions
+import org.jetbrains.dokka.base.DokkaBase
+import org.jetbrains.dokka.plugability.DokkaPlugin
+import org.jetbrains.dokka.stdlibPlugin.merger.StdlibDocumentableMerger
+
+class DokkaStdlibPlugin : DokkaPlugin() {
+
+    val mergerer by extending {
+        CoreExtensions.documentableMerger providing ::StdlibDocumentableMerger override plugin<DokkaBase>().documentableMerger
+    }
+}
